6b8486f1c5ea4880e0e0cf7b60baa497f6a44e0d
Antonio Alves <a50539@alunos.isel.pt>
2025-04-02 15:29:08 +0100

    Remove AUTH_MICROSOFT_ENTRA_ID_ISSUER from environment configuration and authentication setup

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -6,13 +6,7 @@ export const { handlers } = NextAuth({
   providers: [
     MicrosoftEntraID({
       clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID,
-      issuer: process.env.AUTH_MICROSOFT_ENTRA_ID_ISSUER,
       clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
-      authorization: {
-        params: {
-          scope: "openid profile email User.Read",
-        },
-      },
     }),
   ],
   secret: process.env.AUTH_SECRET
