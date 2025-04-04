03ee7ad754141ecb7d243db565dc356a5e902b31
50565 <a50565@alunos.isel.pt>
2025-04-04 22:01:35 +0100

    Enhance dashboard page; update lab data structure and add lab action buttons for teachers; improve layout with dividers and card components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,16 @@
-import { PlusOutlined } from '@ant-design/icons';
-import { Button, Card, Flex, Layout, Tooltip } from 'antd';
+import { PlusOutlined, SettingOutlined } from '@ant-design/icons';
+import { Button, Card, Divider, Flex, Layout, Tooltip } from 'antd';
 import Title from 'antd/es/typography/Title';
 import Link from 'next/link';
 import { auth } from '~/server/auth';
 
 const labsWidth = 300;
 
-const labs = [
-  {
-    title: 'Lab 1',
-    description: 'Test',
-  },
-  {
-    title: 'Lab 2',
-    description: 'Test',
-  }
-];
+// TODO: Add a function to get the labs from the database
+export const labs = [
+  'FPGA 1',
+  'Laboratório Química 1'
+]
 
 const cardStyle = {
   width: labsWidth,
@@ -28,24 +23,38 @@ export default async function Dashboard() {
   const session = await auth();
   const { tempRole } = session!.user;
 
+
+  function getLabActions(labId: number) {
+    return [
+      (
+        <Tooltip title="Editar laboratório" key="settings">
+          <Link href={`/lab/${labId}/settings`}>
+            <SettingOutlined/>
+          </Link>
+        </Tooltip>
+      ),
+    ]
+}
+
   return (
     <Layout style={{ padding: "1%" }}>
       <Title level={2}>Os meus laboratórios</Title>
+      <Divider/>
       <Flex wrap gap="small">
         {labs.map((lab, index) => {
           const labId = index + 1;
           return (
-            <Tooltip title={lab.title} key={labId}>
+            <Card
+              key={labId}
+              style={cardStyle}
+              actions={tempRole === "teacher" ? getLabActions(labId) : []}
+            >
               <Link href={`/lab/${labId}`} key={labId}>
-                <Card
-                  key={labId}
-                  title={lab.title}
-                  style={cardStyle}
-                >
-                  <p>{lab.description}</p>
-                </Card>
+                <Title level={4} style={{ textAlign: "center" }}>
+                  {lab}
+                </Title>
               </Link>
-            </Tooltip>
+            </Card>
           )
         })}
         {tempRole === "teacher" && (
